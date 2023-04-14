3fac789a6725198306ace92626a0580d898a3159
DeveloperAniket <i.aniket.samanta@gmail.com>
2023-04-14 18:33:06 +0530

    Update ConnectionsString.cs

--- src/DbContexts/ConnectionsString.cs
+++ src/DbContexts/ConnectionsString.cs
@@ -7,7 +7,7 @@ namespace ProductService.ProductAPI.DbContexts
     public static class ConnectionsString
     {
         private static string  username = "aniketmaster";
-        private static string password = "Aniket123";
+        private static string password = "*****";
         private static string hostname = "productmaster.c4jksrdqtjxw.us-west-2.rds.amazonaws.com";
         private static string port = "5433";
         private static string dbname = "ProductDB";
